d88d11f673c42f5d0ac0cb1710905ba440d905f1
Rohitas_b <bansalrohitas44@gmail.com>
2022-05-27 17:02:57 +0530

    Mostly commenting and also commenting my doubts with //?

--- src/alicepda.ts
+++ src/alicepda.ts
@@ -1,4 +1,8 @@
+/* Importing the AccountLayout, Token, and TOKEN_PROGRAM_ID from the spl-token library. */
 import { AccountLayout, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+
+/* Importing the Connection, Keypair, PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, Transaction, and
+TransactionInstruction from the @solana/web3.js library. */
 import {
   Connection,
   Keypair,
@@ -8,6 +12,7 @@ import {
   Transaction,
   TransactionInstruction,
 } from "@solana/web3.js";
+
 import BN = require("bn.js");
 import {
   EscrowLayout,
@@ -19,37 +24,70 @@ import {
   writePublicKey,
 } from "./utils";
 
+console.log(" Alice starts ");
 const alice = async () => {
-  const escrowProgramId = getProgramId();
 
+  // * GET PUBKEY {
+
+  // Get Pubkey of Escrow Program
+  const escrowProgramId = getProgramId(); 
+  
+  // Get Pubkey of Alice's Token X account
   const aliceXTokenAccountPubkey = getPublicKey("alice_x");
+  
   // const aliceYTokenAccountPubkey = getPublicKey("alice_y");
-  const XTokenMintPubkey = getPublicKey("mint_x");
-  const aliceKeypair = getKeypair("alice");
 
+  // get pubkey of Token X Mint Account
+  const XTokenMintPubkey = getPublicKey("mint_x");
+  
+  // * GET KEYPAIR {
+
+  // get keypair of Alice account //? We don't Alice Y token account because Y is SOL, right ??
+  const aliceKeypair = getKeypair("alice");
+  
+  // get keypair of Temp token x account
   const tempXTokenAccountKeypair = new Keypair();
+  
+  // Establish Json RPC connection with devnet
   const connection = new Connection(
     "https://api.devnet.solana.com",
     "confirmed"
   );
-  // Empty account to hold NFT
+
+  /* 
+    Alice's transaction consists of 5 instructions.
+
+    1. create empty account owned by token program
+    2. initialize empty account as Alice's X token account
+    3. transfer X tokens from Alice's main X token account to her temporary X token account
+    4. create empty account owned by escrow program
+    5. initialize empty account as escrow state and transfer temporary X token account ownership to PDA
+  */
+
+  // * 1 Empty account to hold NFT rented out by Alice
+  // Creating a new account for the temporary token account.
+  // const tempXTokenAccount = new Account(); //? Why not create new temp Token Account 
   const createTempTokenAccountIx = SystemProgram.createAccount({
     programId: TOKEN_PROGRAM_ID,
     space: AccountLayout.span,
     lamports: await connection.getMinimumBalanceForRentExemption(
       AccountLayout.span
     ),
-    fromPubkey: aliceKeypair.publicKey,
-    newAccountPubkey: tempXTokenAccountKeypair.publicKey,
+    fromPubkey: aliceKeypair.publicKey, // pays rent for account space
+    newAccountPubkey: tempXTokenAccountKeypair.publicKey, //? How do we already know pk of account that has yet to be created
   });
+
   console.log("Step1");
+  // * 2 Initializing the temporary token account. 
   const initTempAccountIx = Token.createInitAccountInstruction(
     TOKEN_PROGRAM_ID,
     XTokenMintPubkey,
     tempXTokenAccountKeypair.publicKey,
     aliceKeypair.publicKey
   );
+  
   console.log("Step2");
+  // * 3 Transferring 1 token from Alice's X token account to the temporary token account.
   const transferXTokensToTempAccIx = Token.createTransferInstruction(
     TOKEN_PROGRAM_ID,
     aliceXTokenAccountPubkey,
@@ -58,20 +96,23 @@ const alice = async () => {
     [],
     1
   );
+
   console.log("Step3");
-  // eslint-disable-next-line no-unused-vars
+  // * Finding the program address of the escrow program. 
   const [pda, _nounce] = await PublicKey.findProgramAddress(
-    [XTokenMintPubkey.toBuffer()],
+    [XTokenMintPubkey.toBuffer()], //? What is buffer representation 
     escrowProgramId
   );
+
   // const transferSolRentToPDA = SystemProgram.transfer({
   //   fromPubkey: aliceKeypair.publicKey,
   //   toPubkey: pda,
   //   lamports: await connection.getMinimumBalanceForRentExemption(
   //     ESCROW_ACCOUNT_DATA_LAYOUT.span
   //   ),
-  // });
-  // const escrowKeypair = new Keypair();
+  // }); //? Why comment out? Why is it not reqd ?
+
+  // const escrowKeypair = new Keypair(); //? Why is new Escrow Account not created ?
   // const createEscrowAccountIx = SystemProgram.createAccount({
   //   space: ESCROW_ACCOUNT_DATA_LAYOUT.span,
   //   lamports: await connection.getMinimumBalanceForRentExemption(
@@ -81,11 +122,16 @@ const alice = async () => {
   //   newAccountPubkey: pda,
   //   programId: escrowProgramId,
   // });
+
   console.log("Step4");
+  // * 5 Creating a new transaction instruction.
   const initEscrowIx = new TransactionInstruction({
     programId: escrowProgramId,
     keys: [
-      { pubkey: aliceKeypair.publicKey, isSigner: true, isWritable: false },
+      { pubkey: aliceKeypair.publicKey, 
+        isSigner: true, 
+        isWritable: false 
+      },
       {
         pubkey: tempXTokenAccountKeypair.publicKey,
         isSigner: false,
@@ -96,16 +142,25 @@ const alice = async () => {
         isSigner: false,
         isWritable: false,
       },
-      { pubkey: pda, isSigner: false, isWritable: true },
-      { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
-      { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: pda, 
+        isSigner: false, 
+        isWritable: true 
+      },
+      { pubkey: SYSVAR_RENT_PUBKEY, // Systen Var for Rent
+        isSigner: false, 
+        isWritable: false 
+      },
+      { pubkey: TOKEN_PROGRAM_ID, // Token Program
+        isSigner: false, 
+        isWritable: false 
+      },
       {
-        pubkey: new PublicKey("11111111111111111111111111111111"),
+        pubkey: new PublicKey("11111111111111111111111111111111"), // System Program
         isSigner: false,
         isWritable: false,
       },
     ],
-    data: Buffer.from(
+    data: Buffer.from(  //? unclear of what's happening
       Uint8Array.of(
         0,
         ...new BN(1).toArray("le", 8),
@@ -114,7 +169,9 @@ const alice = async () => {
       )
     ),
   });
+
   console.log("Step5");
+  //  Creating the transaction
   const tx = new Transaction().add(
     createTempTokenAccountIx,
     initTempAccountIx,
@@ -122,27 +179,34 @@ const alice = async () => {
     // createEscrowAccountIx,
     initEscrowIx
   );
+
   console.log("Sending Alice's transaction...");
+  //  Sending the Transaction 
   await connection.sendTransaction(
     tx,
     [aliceKeypair, tempXTokenAccountKeypair],
     { skipPreflight: false, preflightCommitment: "confirmed" }
   );
+
   console.log("Step6");
   // sleep to allow time to update
   await new Promise((resolve) => setTimeout(resolve, 5000));
+
   console.log("Step7");
+  // Fetch all the account info for pda (specific pubkey)
   const escrowAccount = await connection.getAccountInfo(pda);
 
   if (escrowAccount === null || escrowAccount.data.length === 0) {
-    logError("Escrow state account has not been initialized properly");
+    logError("Escrow state account has not been initialized properly"); //? Why not have its own custom Error 
     process.exit(1);
   }
 
-  const encodedEscrowState = escrowAccount.data;
+  const encodedEscrowState = escrowAccount.data; // data is currently encoded
   const decodedEscrowState = ESCROW_ACCOUNT_DATA_LAYOUT.decode(
     encodedEscrowState
-  ) as EscrowLayout;
+  ) as EscrowLayout; // decode according to EscrowLayout schema
+
+  // Verify correctness of decoded Escrow data
   console.log("Step8");
   if (!decodedEscrowState.isInitialized) {
     logError("Escrow state initialization flag has not been set");
@@ -175,7 +239,8 @@ const alice = async () => {
     );
     process.exit(1);
   }
-  writePublicKey(pda, "escrow");
+  writePublicKey(pda, "escrow"); // Store Pubkey of PDA in file
+
   // console.table([
   //   {
   //     "Alice Token Account X": await getTokenBalance(
@@ -204,4 +269,4 @@ const alice = async () => {
   console.log("complete");
 };
 
-alice();
+alice();  //? Can we shift it at the top ?
--- src/available.ts
+++ src/available.ts
@@ -1,56 +1,56 @@
-import { Connection, PublicKey } from "@solana/web3.js";
-import BN = require("bn.js");
-import base64;
-import {
-  EscrowLayout,
-  ESCROW_ACCOUNT_DATA_LAYOUT,
-  getProgramId,
-  getPublicKey,
-  logError,
-} from "./utils";
+// import { Connection, PublicKey } from "@solana/web3.js";
+// import BN = require("bn.js");
+// import base64;
+// import {
+//   EscrowLayout,
+//   ESCROW_ACCOUNT_DATA_LAYOUT,
+//   getProgramId,
+//   getPublicKey,
+//   logError,
+// } from "./utils";
 
-const bob = async () => {
-  const escrowProgramId = getProgramId();
-  const connection = new Connection(
-    "https://api.devnet.solana.com",
-    "confirmed"
-  );
-  const account = await connection.getParsedProgramAccounts(escrowProgramId, {
-    filters: [
-      {
-        memcmp: {
-          offset: 177,
-          bytes: base64.b64encode(0), // base58 encoded string
-        },
-      },
-    ],
-  });
-  //account=null if not found
-  const encodedEscrowState = account[0].account.data;
-  const decodedEscrowLayout = ESCROW_ACCOUNT_DATA_LAYOUT.decode(
-    encodedEscrowState
-  ) as EscrowLayout;
-  const escrowState = {
-    escrowAccountPubkey: escrowProgramId.toBase58(),
-    isInitialized: !!decodedEscrowLayout.isInitialized,
-    initializerAccountPubkey: new PublicKey(
-      decodedEscrowLayout.initializerPubkey
-    ).toBase58(),
-    XTokenTempAccountPubkey: new PublicKey(
-      decodedEscrowLayout.initializerTempTokenAccountPubkey
-    ).toBase58(),
-    initializerRentRecieveAccount: new PublicKey(
-      decodedEscrowLayout.initializerReceivingTokenAccountPubkey
-    ).toBase58(),
-    tokenPubkey: new PublicKey(decodedEscrowLayout.tokenPubkey).toBase58(),
-    rate: new BN(decodedEscrowLayout.rate, 10, "le"),
-    expiry: new BN(decodedEscrowLayout.expiry, 10, "le"),
-    rentee: new PublicKey(decodedEscrowLayout.rentee).toBase58(),
-    state: new BN(decodedEscrowLayout.state),
-    min_duration: new BN(decodedEscrowLayout.min_duration, 10, "le"),
-    max_duration: new BN(decodedEscrowLayout.max_duration, 10, "le"),
-  };
-  console.log("pda: ", escrowState);
-};
+// const bob = async () => {
+//   const escrowProgramId = getProgramId();
+//   const connection = new Connection(
+//     "https://api.devnet.solana.com",
+//     "confirmed"
+//   );
+//   const account = await connection.getParsedProgramAccounts(escrowProgramId, {
+//     filters: [
+//       {
+//         memcmp: {
+//           offset: 177,
+//           bytes: base64.b64encode(0), // base58 encoded string
+//         },
+//       },
+//     ],
+//   });
+//   //account=null if not found
+//   const encodedEscrowState = account[0].account.data;
+//   const decodedEscrowLayout = ESCROW_ACCOUNT_DATA_LAYOUT.decode(
+//     encodedEscrowState
+//   ) as EscrowLayout;
+//   const escrowState = {
+//     escrowAccountPubkey: escrowProgramId.toBase58(),
+//     isInitialized: !!decodedEscrowLayout.isInitialized,
+//     initializerAccountPubkey: new PublicKey(
+//       decodedEscrowLayout.initializerPubkey
+//     ).toBase58(),
+//     XTokenTempAccountPubkey: new PublicKey(
+//       decodedEscrowLayout.initializerTempTokenAccountPubkey
+//     ).toBase58(),
+//     initializerRentRecieveAccount: new PublicKey(
+//       decodedEscrowLayout.initializerReceivingTokenAccountPubkey
+//     ).toBase58(),
+//     tokenPubkey: new PublicKey(decodedEscrowLayout.tokenPubkey).toBase58(),
+//     rate: new BN(decodedEscrowLayout.rate, 10, "le"),
+//     expiry: new BN(decodedEscrowLayout.expiry, 10, "le"),
+//     rentee: new PublicKey(decodedEscrowLayout.rentee).toBase58(),
+//     state: new BN(decodedEscrowLayout.state),
+//     min_duration: new BN(decodedEscrowLayout.min_duration, 10, "le"),
+//     max_duration: new BN(decodedEscrowLayout.max_duration, 10, "le"),
+//   };
+//   console.log("pda: ", escrowState);
+// };
 
-bob();
+// bob();
